8a3eb638c6b68a4fe246ac7fd3a966ea218ef35f
Shawn Pan <shaw@sitecore.net>
2026-08-04 15:43:12 +0800

    Update image component

--- examples/kit-nextjs-skate-park/src/components/image/Image.tsx
+++ examples/kit-nextjs-skate-park/src/components/image/Image.tsx
@@ -4,10 +4,14 @@ import {
 } from "@sitecore-content-sdk/nextjs";
 import React from "react";
 import { CompatibleLink } from "components/content-sdk/CompatibleLink";
-import { getFieldValue } from 'lib/component-props';
-import { ImageProps, ImageWrapperProps } from './image.props';
+import { getFieldValue } from "lib/component-props";
+import { ImageProps, ImageWrapperProps } from "./image.props";
 
-const ImageWrapper: React.FC<ImageWrapperProps> = ({ className, id, children }) => (
+const ImageWrapper: React.FC<ImageWrapperProps> = ({
+  className,
+  id,
+  children,
+}) => (
   <figure className={className.trim()} id={id}>
     <div className="component-content">{children}</div>
   </figure>
@@ -21,7 +25,7 @@ const ImageDefault: React.FC<ImageProps> = ({ params }) => (
 
 export const Banner: React.FC<ImageProps> = ({ params, fields }) => {
   const { styles, RenderingIdentifier: id } = params;
-  const baseImageField = getFieldValue(fields.Image);
+  const baseImageField = getFieldValue(fields?.Image);
   const imageField = baseImageField && {
     ...baseImageField,
     value: {
@@ -42,7 +46,10 @@ export const Banner: React.FC<ImageProps> = ({ params, fields }) => {
     "(max-width: 640px) 100vw, (max-width: 768px) 768px, (max-width: 1024px) 1024px, (max-width: 1440px) 1280px, 1920px";
 
   return (
-    <figure className={`component hero-banner ${styles}`.trim()} id={typeof id === "string" ? id : undefined}>
+    <figure
+      className={`component hero-banner ${styles}`.trim()}
+      id={typeof id === "string" ? id : undefined}
+    >
       <div className="component-content sc-sxa-image-hero-banner">
         <ContentSdkImage
           field={imageField}
@@ -72,9 +79,7 @@ export const Default: React.FC<ImageProps> = (props) => {
       field={imageField}
       sizes="(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 90vw, 1200px"
       alt={
-        typeof imageField?.value?.alt === "string"
-          ? imageField.value.alt
-          : ""
+        typeof imageField?.value?.alt === "string" ? imageField.value.alt : ""
       }
     />
   );
@@ -82,7 +87,10 @@ export const Default: React.FC<ImageProps> = (props) => {
     !page?.mode?.isEditing && targetUrlField?.value?.href;
 
   return (
-    <ImageWrapper className={`component image ${styles}`} id={typeof id === "string" ? id : undefined}>
+    <ImageWrapper
+      className={`component image ${styles}`}
+      id={typeof id === "string" ? id : undefined}
+    >
       {shouldWrapWithLink ? (
         <CompatibleLink field={targetUrlField}>
           <Image />
